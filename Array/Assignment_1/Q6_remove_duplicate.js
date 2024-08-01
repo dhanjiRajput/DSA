6841d19e4fe8aa0b83a3a26eab4169ee5ce10f3a
dhanjiRajput <kidechadhanji@gmail.com>
2024-08-01 19:22:39 +0100

    updated

--- Array/Assignment_1/Q6_remove_duplicate.js
+++ Array/Assignment_1/Q6_remove_duplicate.js
@@ -1,2 +1,19 @@
 let arr=[1,2,3,4,5,2,2,1];
 let no_duplicate=[];
+
+const isExist=(val)=>{
+    for(let i=0;i<arr.length;i++){
+        if(no_duplicate[i]==val){
+            return true;
+        }
+    }
+    return false;
+}
+
+for(let i=0;i<arr.length;i++){
+    if(!isExist(arr[i])){
+        no_duplicate.push(arr[i]);
+    }
+}
+
+console.log(no_duplicate);
