8de3b1359a2f17211ad976473156fd6819d99100
Dhanji Rajput <147084804+dhanjiRajput@users.noreply.github.com>
2024-08-12 18:52:53 +0530

    Exam Update

---
+++ Exam_2/Q2_target_sum.js
@@ -0,0 +1,11 @@
+
+let arr=[1,2,3,4,5];
+let target=6;
+
+for(let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+        if(arr[i]+arr[j]<target){
+            console.log(arr[i],arr[j]);
+        }
+    }
+}
