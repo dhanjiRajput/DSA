d6cafd303c3243b1a9b4312bf8fd267364816601
dhanjiRajput <kidechadhanji@gmail.com>
2024-08-21 18:50:42 +0530

    updated

--- Exam_3/Q1_string_comparison.js
+++ Exam_3/Q1_string_comparison.js
@@ -1,30 +1,15 @@
-
-const comparison=(str1,str2)=>{
-    let arr1=[...str1];
-    let arr2=[...str2];
-    arr1.sort();
-    arr2.sort();
-    let l1=arr1.length;
-    let l2=arr2.length;
-
-    let flag=false;
-
-    if(l1==l2){
-        for(let i=0;i<arr1.length;i++){
-            for(let j=0;j<arr2.length;j++){
-                if(arr1[i]==arr2[j]){
-                    return true;
-                }else{
-                    return false;   
-                }
-            }
-        }
-    }else{
-        return false;
-    }
-}
 let str1="hello";
 let str2="elloh";
 
-console.log(comparison(str1,str2));
+str1=str1.split("").sort().join();
+str2=str2.split("").sort().join();
 
+if(str1.length===str2.length){
+    if(str1==str2){
+        console.log("true");
+    }else{
+        console.log("false");
+    }
+}else{
+    console.log("false");
+}
