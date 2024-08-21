c6050efeea0a9571b5155eb4cea6d508a01e7e88
dhanjiRajput <kidechadhanji@gmail.com>
2024-08-21 19:05:43 +0530

    updated

--- Exam_3/Q1_string_comparison.js
+++ Exam_3/Q1_string_comparison.js
@@ -5,7 +5,7 @@ str1=str1.split("").sort().join();
 str2=str2.split("").sort().join();
 
 if(str1.length===str2.length){
-    if(str1==str2){
+    if(str1===str2){
         console.log("true");
     }else{
         console.log("false");
